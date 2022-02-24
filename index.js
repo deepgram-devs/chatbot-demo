fa4801e055c7a98e8af6d4a974d5e56fb7c379fb
Kevin Lewis <kvn@lws.io>
2022-02-24 19:01:31 +0000

    Express setup, /ask works

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
+require('dotenv').config()
+const express = require('express')
 const { dockStart } = require('@nlpjs/basic');
 
-(async () => {
-  const dock = await dockStart();
-  const nlp = dock.get('nlp');
-  const response = await nlp.process('en', 'sa ya chuck norris joke');
-  console.log(response);
-})();
+const app = express()
+app.use(express.json())
+
+dockStart().then(dock => (nlp = dock.get('nlp')))
+
+app.get('/ask', async (req, res) => {
+    try {
+        const { answer } = await nlp.process('en', req.query.prompt);
+        res.json({ answer })
+    } catch(error) {
+        res.json({ error })
+    }
+})
+
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, console.log(`Started on *:${PORT} at ${new Date().toISOString()}`))
